3258001920d98c5cb049f6716aba8489a511bc5f
Abu Younus <93146335+abuyounus72@users.noreply.github.com>
2021-11-07 23:30:48 +0600

    Add files via upload

---
+++ Project_2/js/main.js
@@ -0,0 +1,12 @@
+// bx slider js
+// $(document).ready(function(){
+//     $('.slider_area').bxSlider();
+//   });
+
+// menu menu active
+// jQuery(document).ready(function () {
+// 	jQuery('#main_menu').meanmenu({
+//         meanMenuContainer: '#mean_menu',
+//         meanScreenWidth: 768,
+//     });
+// });
